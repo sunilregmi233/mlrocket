a1498c06949179462484e49a1a1f9f53a387fabf
Sunil <sunilregmi223@gmail.com>
2018-02-12 15:02:06 +0545

    First commit

---
+++ sketch.js
@@ -0,0 +1,46 @@
+var population;
+// Each rocket is alive till 400 frames
+var lifespan = 400;
+// Made to display count on screen
+var lifeP;
+// Keeps track of frames
+var count = 0;
+var genn = 0;
+// Where rockets are trying to go
+var target;
+// Max force applied to rocket
+var maxforce = 0.2;
+
+// Dimensions of barrier
+var rx = 100;
+var ry = 150;
+var rw = 200;
+var rh = 10;
+
+function setup() {
+  createCanvas(500, 600);
+  population = new Population();
+  lifeP = createP();
+  target = createVector(width / 2, 50);
+
+}
+
+function draw() {
+  background(0);
+  population.run();
+  // Displays count to window
+  lifeP.html(genn);
+  count++;
+  if (count == lifespan) {
+    population.evaluate();
+    population.selection();
+
+    count = 0;
+    genn++;
+  }
+  // Renders barrier for rockets
+  fill(255);
+  rect(rx, ry, rw, rh);
+  // Renders target
+  ellipse(target.x, target.y, 16, 16);
+}
